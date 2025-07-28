3d3b8733a885449e0cfe3f9375055e74e5a382fd
shresth_s <161724722+Shresth860@users.noreply.github.com>
2025-07-28 14:28:23 +0000

    initial commit

---
+++ conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+console.log(typeof score);
+
+
+// Conversion
+let stringToNumber = Number(score)
+console.log(stringToNumber);  // output:NAN (not a number)
+
+//"33"=33
+//"33abc"=NAN
+
+let IsLoggedIn = "shresth"
+let BooleanInIsLoggedIn = Boolean(IsLoggedIn)
+console.log(BooleanInIsLoggedIn);
+
+//"string"=true
+//""=0
+
+let number = 33
+let numberToString = String(number)
+console.log(numberToString);
+console.log(typeof numberToString);
+
+// 33="33"
