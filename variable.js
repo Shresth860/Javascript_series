c81a9e60e465ade0dac46b37158fb114ad80b227
shresth_s <161724722+codeWizard860@users.noreply.github.com>
2025-07-28 06:06:25 +0000

    Variable file commit

---
+++ variable.js
@@ -0,0 +1,22 @@
+const accountId = 1245
+
+let accountMail = "shresth@google.com"
+
+var accountPassword = "4512"
+
+// let try to change accountId and then run it(error:: Assignment to constant variable means Not Possible)
+// accountId = 1254
+
+//let try to change accountMail and then run it(Possible hai No error)
+accountMail = "saxena@google.com"
+
+
+//let try to change accountPassword and then run it(possible hai no error)
+accountPassword = "5646"
+
+
+// console.log(accountId)
+// console.log(accountMail)
+// console.log(accountPassword)
+
+console.table([accountId, accountMail, accountPassword]);
